e6adff41a85941b07e1698c66dad4449450e0f6b
Robert burner Schadek <rburners@gmail.com>
2014-04-01 18:12:43 +0200

    forgot one file

---
+++ makedb.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ -f testtable.db ]; then
+	rm testtable.db
+else
+	echo "Args"
+fi
+
+sqlite3 testtable.db "CREATE TABLE Person(Firstname varchar, Lastname varchar, Company varchar, Address varchar, County varchar, City varchar, State varchar, Zip integer, PhoneWork varchar, PhonePrivat varchar, Mail varchar, Www varchar, PRIMARY KEY(Firstname, Lastname, Company));"
